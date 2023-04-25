cd8740cff5fb0c7638824b82ddd680f8400d1d8b
Gokay Terzi <gokayterzi@gmail.com>
2023-04-26 01:04:54 +0300

    v3

--- AtmProject.java
+++ AtmProject.java
@@ -2,6 +2,10 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir atm projesidir");
         //version1
+        System.out.println("Java is easy");
+        //version2
+        System.out.println("Learn Java earn moneu");
+        //version3
     }
 
 }
