9cc3e892715282fb8188da5e8637a7e67fae4c22
Gokay Terzi <gokayterzi@gmail.com>
2023-04-26 01:06:04 +0300

    v3

--- AtmProject.java
+++ AtmProject.java
@@ -6,6 +6,8 @@ public class AtmProject {
         //version2
         System.out.println("Learn Java earn moneu");
         //version3
+        System.out.println("git ogrenmek cok kolay");
+        //version4
     }
 
 }
